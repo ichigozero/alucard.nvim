2fa938b00eac84d4aa6c32749b3ca8edbbba8d6e
Gary Sentosa <gary.sentosa@gmail.com>
2022-11-22 08:46:47 +0900

    feat: monochrome colors

--- lua/lualine/themes/alucard-nvim.lua
+++ lua/lualine/themes/alucard-nvim.lua
@@ -4,30 +4,30 @@ local colors = require("alucard").colors()
 local bg = colors.black
 
 alucard.normal = {
-    a = { fg = colors.black, bg = colors.purple, gui = "bold" },
-    b = { fg = colors.purple, bg = bg },
+    a = { fg = colors.black, bg = colors.gray, gui = "bold" },
+    b = { fg = colors.white, bg = bg },
     c = { fg = colors.white, bg = bg },
 }
 
 alucard.visual = {
-    a = { fg = colors.black, bg = colors.pink, gui = "bold" },
-    b = { fg = colors.pink, bg = bg },
+    a = { fg = colors.black, bg = colors.gray, gui = "bold" },
+    b = { fg = colors.white, bg = bg },
 }
 
 alucard.inactive = {
-    a = { fg = colors.white, bg = colors.gray, gui = "bold" },
+    a = { fg = colors.white, bg = colors.dark_gray, gui = "bold" },
     b = { fg = colors.black, bg = colors.white },
 }
 
 alucard.replace = {
-    a = { fg = colors.black, bg = colors.yellow, gui = "bold" },
-    b = { fg = colors.yellow, bg = bg },
+    a = { fg = colors.black, bg = colors.gray, gui = "bold" },
+    b = { fg = colors.white, bg = bg },
     c = { fg = colors.white, bg = bg },
 }
 
 alucard.insert = {
-    a = { fg = colors.black, bg = colors.green, gui = "bold" },
-    b = { fg = colors.green, bg = bg },
+    a = { fg = colors.black, bg = colors.gray, gui = "bold" },
+    b = { fg = colors.white, bg = bg },
     c = { fg = colors.white, bg = bg },
 }
 
